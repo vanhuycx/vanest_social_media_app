61496aeb5c782e08dda4d305d7cf1bc4a7a9a54f
vanhuycx <vanhuycx@gmail.com>
2022-03-23 12:29:09 -0400

    Update credentials for MongoDB. Update node server to use .env credentials

--- server/index.js
+++ server/index.js
@@ -2,18 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 // Set up the express app instance
-
 // Limit the upload to 30mb and set-up parsing URL-encoded data library
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 const CONNECTION_URL =
-  'mongodb+srv://mongodb_user1:Mongodb_user1_2022%21%40@vanest01.sg1hz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  'mongodb+srv://' +
+  process.env.MONDGODB_USER +
+  ':' +
+  process.env.MONDGODB_PASS +
+  '@vanest01.sg1hz.mongodb.net/VanestDB?retryWrites=true&w=majority';
 
 const PORT = process.env.PORT || 5000;
 
