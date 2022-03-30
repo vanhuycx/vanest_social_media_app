1a2e94a22e9067b154c29b72872da26b9daed7d5
vanhuycx <vanhuycx@gmail.com>
2022-03-30 10:47:19 -0400

    Edit Posts and Form components. Add grid container

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import vanestImage from './images/vanest.png';
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
-
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 const App = () => {
   return (
     <>
@@ -10,14 +11,23 @@ const App = () => {
           {/* <Typography variant='h2' align='center'>
             Vanest
           </Typography> */}
-          <img src={vanestImage}   alt='' />
+          <img src={vanestImage} height={200} width={200} alt='' />
         </AppBar>
         <Grow in>
           <Container>
-            <Grid container justify='space-between' alignItems='stretch' spacing={3}>
-
+            <Grid
+              container
+              justify='space-between'
+              alignItems='stretch'
+              spacing={3}
+            >
+              <Grid item xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
             </Grid>
-
           </Container>
         </Grow>
       </Container>
