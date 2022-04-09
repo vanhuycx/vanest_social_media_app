5af22268bfaf818f200ed72201fdc18f9172285e
vanhuycx <vanhuycx@gmail.com>
2022-04-09 08:44:00 -0400

    Build the Form

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextField, Button, Typography, Paper } from '@mui/material';
 
 const Form = () => {
-  return <h1>Form</h1>;
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedFile: '',
+  });
+
+  const handleSubmit = () => {};
+  return (
+    <>
+      <Paper className='paper'>
+        <form
+          className='form'
+          noValidate
+          onSubmit={handleSubmit}
+          autoComplete='off'
+        >
+          <Typography variant='h6'>Creating a post</Typography>
+          <TextField
+            name='creator'
+            variant='outlined'
+            label='Creator'
+            fullWidth
+            value={postData.creator}
+            onChange={(e) =>
+              setPostData({ ...postData, creator: e.target.value })
+            }
+          ></TextField>
+        </form>
+      </Paper>
+    </>
+  );
 };
 
 export default Form;
